6984ec8ac0d7c2133fe532e32b77bccc51bb5646
Ubuntu <bithika@amygb.ai>
2024-05-30 13:48:51 +0000

    Test GitHub Actions2

--- LLM_INTERFACE/extract_entities.py
+++ LLM_INTERFACE/extract_entities.py
@@ -30,7 +30,7 @@ class LLM_interface:
 
         path = Path( self.src_folder_ )
         llm_input_ = [str(file.resolve()) for file in path.rglob('*') if file.is_file()]
-        print('Change for testing github actions#2')
+        print('Change for testing github actions#3')
 
         return llm_input_
 
