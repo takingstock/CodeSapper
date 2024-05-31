255e95c4aaa2013908107fd6049b6cbadd79bb8f
Ubuntu <bithika@amygb.ai>
2024-05-31 12:59:16 +0000

    Trigger workflow addition#66

--- IKG/download_artefacts.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python download_artefacts.py <run_id>")
-        sys.exit(1)
-
-    diff_file = sys.argv[1]
-    print('THOR RUNN IDDD->', diff_file)
